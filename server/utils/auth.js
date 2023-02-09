e237f1a047b657d5a59cb1e5a8202132e3e0cc04
unknown <peterlim995@gmail.com>
2023-02-08 21:16:18 -0500

    server.js auth.js update

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -8,16 +8,20 @@ module.exports = {
   // function for our authenticated routes
   authMiddleware: function (req, res, next) {
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
 
-    if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+    if(!token) {
+      return req;
     }
+
+    // if (!token) {
+    //   return res.status(400).json({ message: 'You have no token!' });
+    // }
 
     // verify token and get user data out of it
     try {
@@ -25,11 +29,13 @@ module.exports = {
       req.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+      // return res.status(400).json({ message: 'invalid token!' });
     }
 
     // send to next endpoint
-    next();
+    // next();
+
+    return req;
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
